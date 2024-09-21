03c62b57d8f0a99fa55cb2c40f19cc2c34f3ad66
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-09-21 18:29:35 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <>
+       <h1>GOVERNOR IT INITIATIVE</h1>
+       <h2>Hello World</h2>
+       <p>It is my first web page</p>
+       <p className="p2">Made By <span>ZAHIDA ABBASI</span></p>
+    </>
+    
+    
+  )
+}
